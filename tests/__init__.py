8572bbab5e5c406d905f41fa136c2a36d52bb21c
Louis-Vincent Grand'Maison <lvgra@ulaval.ca>
2026-01-09 17:52:52 -0500

    add testing

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/tests/__init__.py
-#
-# Copyright 2024-2026 Louis-Vincent Grand'Maison
-# This software is dual-licensed under the MIT and Apache-2.0 licenses.
-# See the NOTICE file for more information.
