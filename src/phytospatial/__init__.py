bee08d5098385bdff13de0f4aefacac4c8631cb1
Louis-Vincent Grand'Maison <lvgra@ulaval.ca>
2026-01-08 21:11:12 -0500

    trigger gh action

--- src/phytospatial/__init__.py
+++ src/phytospatial/__init__.py
@@ -2,7 +2,7 @@
 #
 # Copyright 2024-2026 Louis-Vincent Grand'Maison
 # This software is dual-licensed under the MIT and Apache-2.0 licenses.
-# See the NOTICE file for more information.
+# See the NOTICE file for more information
 
 import logging
 
