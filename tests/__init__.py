de25b221b10c9c6c55526f70294039d6e5294e3d
Louis-Vincent Grand'Maison <lvgra@ulaval.ca>
2026-01-08 21:01:37 -0500

    test gh action

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# src/tests/__init__.py
+#
+# Copyright 2024-2026 Louis-Vincent Grand'Maison
+# This software is dual-licensed under the MIT and Apache-2.0 licenses.
+# See the NOTICE file for more information.
